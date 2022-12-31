1b07ef5419ca67beb31a167ff6542f1b9b791686
luiz <luizmarcelolm@hotmail.com>
2022-12-30 23:01:56 -0300

    criação de funçoes do layout

--- main.js
+++ main.js
@@ -3,15 +3,11 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () => {
+    clearFilds()
+    document.getElementById('modal').classList.remove('active')
+}
 
-    const tempClient = {
-        nome: "Luiz Marcelo",
-        email: "luizmarcelolm@hotmail.com",
-        celular: "1234556789",
-        cidade: "São José dos Campos"
-    }
 
     const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
     const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
@@ -36,8 +32,68 @@ const closeModal = () => document.getElementById('modal')
         setLocalStorage(dbClient)
     }
 
+    //validação de campos
+    const isValidFields = () => {
+       return document.getElementById('form').reportValidity()
+    }
+
+    //Limpa campos e fecha modal
+    const clearFilds = () => {
+        const fields = document.querySelectorAll('.modal-field')
+        fields.forEach(field => field.value = "")
+    }
+
+    //ação do layout
+    const saveClient = () => {
+        if(isValidFields()) {
+            const client = {
+                nome: document.getElementById('nome').value,
+                email: document.getElementById('email').value,
+                celular: document.getElementById('celular').value,
+                cidade: document.getElementById('cidade').value
+            }
+            createClient(client)
+            clearFilds()
+            //updateTable()
+            closeModal()
+        }
+    }
+
+ const createRow = (client) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+    <td>${client.nome}</td>
+    <td>${client.email}</td>
+    <td>${client.celular}</td>
+    <td>${client.cidade}</td>
+    <td>
+       <button type = "button" class = "button green"> editar</button>
+       <button type = "button" class = "button red"> excluir</button>
+    </td>
+    `
+    document.querySelector('#tableClient>tbody').appendChild(newRow)
+ }   
+
+ const clearTable = () => {
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+ }
+
+const updateTable = () => {
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}   
+
+//updateTable()    
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
+
+ //updateTable() 
